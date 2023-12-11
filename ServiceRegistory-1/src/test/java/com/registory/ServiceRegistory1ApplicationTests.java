db4f3f81b0b4547d3cf54fdb4e302052715d2d07
yogesh <yogeshgawali141@gmail.com>
2023-12-11 21:50:05 +0530

    RegistryServiceCommit

---
+++ ServiceRegistory-1/src/test/java/com/registory/ServiceRegistory1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistory1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
